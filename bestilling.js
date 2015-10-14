22a501f6743b3285c1e08cf9d4311167ff9d1336
Michael <simonmeb2@yahoo.com>
2015-10-14 16:42:48 +0200

    endringer og laget ny mobil stilsett

--- bestilling.js
+++ bestilling.js
@@ -17,20 +17,20 @@
 						display.innerHTML = "Please Select you pizza from the menu ";
 						
 						}
-					if (selectedFoodValue == 1) {    //'<img src="orangepizza.jpg" id="orange" / alt="orange pizza" height="120" width="160">' +
+					if (selectedFoodValue == 1) {   
 							display.innerHTML = " APPLE PIZZA" +  "</br>" +
 							"Ingridents:<br/> Orange, Onion, Ginger, Vodka and some other stuff " + "<br/>PICE:200 kr";
 						}
-					if (selectedFoodValue == 2){    // '<img src="applepizza.jpg" / alt="apple pizza" height="120" width="160">' + "<br/>" +
-							display.innerHTML =  "ORANGE PIZZA" +
+					if (selectedFoodValue == 2){    
+							display.innerHTML =  "ORANGE PIZZA" + "<br/>" +
 							"Ingridents:<br/> Apple, Onion, Ginger, Vodka and some other stuff "  + "<br/>PRICE:200 kr";
 						}
-					if (selectedFoodValue == 3){
-							display.innerHTML =   "PINEAPPLE PIZZA" + '<img src="pineapplepizza.jpg" / alt="pineapple pizza" height="120" width="160">'
-							"<br/>" + "Ingridents:<br/> Pineapple, Onion, Ginger, Vodka and some other stuff "  + "<br/>PRICE:200 kr";
+					if (selectedFoodValue == 3){		//'<img src="pineapplepizza.jpg" / alt="pineapple pizza" height="120" width="160">'
+							display.innerHTML =   "PINEAPPLE PIZZA" + "<br/>" +
+							"Ingridents:<br/> Pineapple, Onion, Ginger, Vodka and some other stuff "  + "<br/>PRICE:200 kr";
 						}
 					if (selectedFoodValue == 4){
-							display.innerHTML =  "CHOCO PIZZA" + '<img src="chocolatepizza.jpg" / alt="chocolate pizza" height="120" width="160">'+"</br>" + 
+							display.innerHTML =  "CHOCO PIZZA" +"</br>" + 
 							"Ingridents:<br/> Chocolate, Onion, Ginger, Vodka and some other stuff"  + "<br/>PRICE:200 kr";
 						}
 					
